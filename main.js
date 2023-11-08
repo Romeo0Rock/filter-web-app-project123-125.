8d1e30d29748c35547a1878e4eaa667d748c2848
Romeo0Rock <116769162+Romeo0Rock@users.noreply.github.com>
2023-11-08 15:18:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(315, 315);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
